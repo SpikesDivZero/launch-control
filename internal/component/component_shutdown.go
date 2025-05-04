b590779cd9ef101047e95a8b5c9aeb274e0d233a
Wesley Spikes <wesley.spikes@gmail.com>
2025-05-04 12:44:53 -0700

    Component shutdown written and tested
    Really not too happy with the quality of this code. I'll probably need
    to revisit this later to figure out why and how to improve it, but for
    now it's better to make progress instead of stalling out.

--- internal/component/component_shutdown.go
+++ internal/component/component_shutdown.go
@@ -1,5 +1,84 @@
 package component
 
-import "context"
+import (
+	"context"
+	"errors"
+	"time"
+)
 
-func (c *Component) Shutdown(context.Context) error { panic("NYI") }
+func (c *Component) Shutdown(ctx context.Context) error {
+	// Stage 1: Prefer a normal shutdown via user-provided ImplShutdown
+	// Stage 2: If that fails, attempt a shutdown via context cancellation.
+	// Stage 3: Give up and abandon the component, so as to not block the app shutdown.
+	//
+	// Each Via function is responsible for checking isDead at the start, in order to keep
+	// testing the main Shutdown() function as simple as possible.
+	c.shutdownViaImpl(ctx)
+	c.shutdownViaContext(ctx)
+	if c.isDead() {
+		return nil
+	} else {
+		return errors.New("component did not respond to ImplShutdown and run context cancel")
+	}
+}
+
+func (c *Component) isDead() bool {
+	select {
+	case <-c.doneCh:
+		return true
+	default:
+		return false
+	}
+}
+
+// Returns nil on success. Error is just for internal test validations.
+func (c *Component) shutdownViaImpl(ctx context.Context) {
+	if c.isDead() {
+		return
+	}
+
+	ctx, ctxCancel := context.WithTimeout(ctx, c.ShutdownOptions.CompletionTimeout)
+	defer ctxCancel()
+
+	// We need to wait for BOTH ImplShutdown to complete, as well as ImplRun to return.
+	//
+	// Why? As a concrete example, take a look at net/http.Shutdown. There, Server.Shutdown causes
+	// Server.Serve to return almost immediately, while Shutdown blocks until the graceful shutdown
+	// process has completed.
+	//
+	// Rather than over-complicate things, this method will focus on calling ImplShutdown and
+	// waiting on it to return. Only after that happens will it check/wait on ImplRun being done.
+
+	// FIXME: use a call grace timeout provided by controller, instead of const 100ms?
+	resultCh := AsyncCall(ctx, c.ShutdownOptions.CallTimeout, 100*time.Millisecond, c.ImplShutdown)
+	if userErr, callErr := (<-resultCh).Values(); callErr != nil {
+		c.logError("ShutdownViaImpl", callErr)
+	} else if userErr != nil {
+		c.logError("ShutdownViaImpl", userErr)
+	}
+
+	select {
+	case <-ctx.Done():
+		// CompletionTimeout expired.
+		c.logError("ShutdownViaImpl", context.Cause(ctx))
+	case <-c.doneCh:
+		// ImplRun finished.
+	}
+}
+
+// Returns nil on success. Error is just for internal test validations.
+func (c *Component) shutdownViaContext(context.Context) {
+	if c.isDead() {
+		return
+	}
+
+	c.runCtxCancel()
+
+	// FIXME: Should we use use ctx? For now, we have a fixed timeout of 100ms.
+	select {
+	case <-c.doneCh:
+		// Responded successfully, and is now exited
+	case <-time.After(100 * time.Millisecond): // FIXME: time provided by controller?
+		c.logError("ShutdownViaContext", errors.New("ImplRun did not respond to shutdown via ctx cancellation; abandoning it"))
+	}
+}
---
+++ internal/component/component_shutdown_test.go
@@ -0,0 +1,226 @@
+package component
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"testing"
+	"testing/synctest"
+	"time"
+
+	"github.com/shoenig/test"
+	"github.com/spikesdivzero/launch-control/internal/testutil"
+)
+
+func TestComponent_Shutdown(t *testing.T) {
+	for _, wantErr := range []bool{false, true} {
+		t.Run(fmt.Sprintf("wantErr %v", wantErr), func(t *testing.T) {
+			synctest.Run(func() {
+				ctx, cancel := context.WithCancelCause(t.Context())
+				defer cancel(errors.New("test done"))
+
+				c := newTestingComponent(t)
+
+				var closeDone func()
+				c.doneCh, closeDone = testutil.ChanWithCloser[struct{}](0)
+
+				// Test goal is only to ensure we call both shutdownVia methods, and that we return an error
+				// if the shutdown did not result in doneCh being closed.
+				//
+				// Specific implementation details of the shutdown methods are out of scope for the main func
+				calls := []string{}
+				c.ImplShutdown = func(ctx context.Context) error {
+					calls = append(calls, "ImplShutdown")
+					return nil
+				}
+				c.runCtxCancel = func() {
+					calls = append(calls, "runCtxCancel")
+					if !wantErr {
+						closeDone()
+					}
+				}
+
+				err := c.Shutdown(ctx)
+				test.Eq(t, []string{"ImplShutdown", "runCtxCancel"}, calls)
+				if wantErr {
+					test.Error(t, err)
+				} else {
+					test.NoError(t, err)
+				}
+			})
+		})
+	}
+}
+
+func TestComponent_isDead(t *testing.T) {
+	c := newTestingComponent(t)
+
+	var closeDone func()
+	c.doneCh, closeDone = testutil.ChanWithCloser[struct{}](0)
+
+	test.False(t, c.isDead())
+	closeDone()
+	test.True(t, c.isDead())
+}
+
+func TestComponent_shutdownViaImpl(t *testing.T) {
+	type control struct {
+		c         *Component
+		closeDone func()
+		cancelCtx context.CancelCauseFunc
+	}
+	type shutdownMock struct {
+		d          time.Duration
+		closesDone bool
+		err        error
+	}
+
+	tests := []struct {
+		name     string
+		control  func(control)
+		shutdown shutdownMock
+		wantD    time.Duration
+	}{
+		{
+			"already dead",
+			func(c control) { c.closeDone() },
+			shutdownMock{d: 5 * time.Second},
+			0,
+		},
+		{
+			"run exits before ImplShutdown returns",
+			nil,
+			shutdownMock{d: 850 * time.Millisecond, closesDone: true},
+			850 * time.Millisecond,
+		},
+		{
+			"ImplShutdown returns before run exits",
+			func(c control) {
+				time.Sleep(773 * time.Millisecond)
+				c.closeDone()
+			},
+			shutdownMock{d: 500 * time.Millisecond},
+			773 * time.Millisecond,
+		},
+		{
+			"ImplShutdown has a user-error", // will log, but not panic
+			nil,
+			shutdownMock{err: errors.New("user error"), closesDone: true},
+			0,
+		},
+		{
+			"ImplShutdown times out",
+			func(c control) {
+				c.c.ShutdownOptions.CallTimeout = 3 * time.Second
+				time.Sleep(4 * time.Second)
+				c.closeDone()
+			},
+			shutdownMock{d: 5 * time.Second},
+			4 * time.Second,
+		},
+		{
+			"shutdown process fails, hits completion timeout",
+			func(c control) {
+				c.c.ShutdownOptions.CompletionTimeout = 2 * time.Second
+			},
+			shutdownMock{d: 3 * time.Second},
+			2*time.Second + 100*time.Millisecond, // FIXME: hard-coded 100ms delay
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			synctest.Run(func() {
+				ctx, cancel := context.WithCancelCause(t.Context())
+				defer cancel(errors.New("test done"))
+
+				c := newTestingComponent(t)
+				ctrl := control{c: c, cancelCtx: cancel}
+
+				c.doneCh, ctrl.closeDone = testutil.ChanWithCloser[struct{}](0)
+
+				shutdownCalled := false
+				c.ImplShutdown = func(ctx context.Context) error {
+					shutdownCalled = true
+					time.Sleep(tt.shutdown.d)
+					if tt.shutdown.closesDone {
+						ctrl.closeDone()
+					}
+					return tt.shutdown.err
+				}
+
+				if tt.control != nil {
+					go tt.control(ctrl)
+					synctest.Wait()
+				}
+
+				t0 := time.Now()
+				c.shutdownViaImpl(ctx)
+				test.Eq(t, tt.wantD, time.Since(t0))
+
+				wantCalled := tt.name != "already dead" // So sue me...
+				test.Eq(t, wantCalled, shutdownCalled)
+			})
+		})
+	}
+}
+
+func TestComponent_shutdownViaContext(t *testing.T) {
+	type control struct {
+		closeDone func()
+	}
+
+	tests := []struct {
+		name    string
+		control func(control)
+		wantD   time.Duration
+	}{
+		{
+			"already dead",
+			func(c control) { c.closeDone() },
+			0,
+		},
+		{
+			"responds within timeout", // FIXME: assumes hard-coded 100ms delay
+			func(c control) {
+				time.Sleep(45 * time.Millisecond)
+				c.closeDone()
+			},
+			45 * time.Millisecond,
+		},
+		{
+			"timeout",
+			nil,
+			100 * time.Millisecond, // FIXME: assumes hard-coded 100ms delay
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			synctest.Run(func() {
+				ctrl := control{}
+
+				c := newTestingComponent(t)
+				c.doneCh, ctrl.closeDone = testutil.ChanWithCloser[struct{}](0)
+
+				calledRunCtxCancel := false
+				c.runCtxCancel = func() {
+					calledRunCtxCancel = true
+				}
+
+				ctx, cancel := context.WithCancelCause(t.Context())
+				defer cancel(errors.New("test done"))
+
+				if tt.control != nil {
+					go tt.control(ctrl)
+					synctest.Wait()
+				}
+
+				t0 := time.Now()
+				c.shutdownViaContext(ctx)
+				test.Eq(t, tt.wantD, time.Since(t0))
+
+				wantCalled := tt.name != "already dead" // So sue me...
+				test.Eq(t, wantCalled, calledRunCtxCancel)
+			})
+		})
+	}
+}
